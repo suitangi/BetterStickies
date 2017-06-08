47f4697cb4107dbac4a930fb3c5b1d3fed96ba2d
suitangi <suitangi777@gmail.com>
2017-06-08 13:08:18 -0500

    Version 1.2.0.0

---
+++ info.java
@@ -0,0 +1,11 @@
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+
+public class info extends JFrame{
+	
+	public info(){
+		ImageIcon icon = new ImageIcon("icon.png");
+        this.setIconImage(icon.getImage());
+	}
+	
+}
